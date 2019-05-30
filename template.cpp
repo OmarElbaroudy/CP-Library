b0749ec3e35c0b7b945680de28a886543b393841
OmarElbaroudy <elbaroudyomar@gmail.com>
2019-05-30 17:30:38 +0200

    template v1

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF=(int)1e9;
+const double EPS=1e-9;
+typedef vector< int > vi;
+typedef vector< vi > vvi;
+typedef pair< int,int > ii;
+typedef vector<ii> vii;
+typedef long long ll;
+#define all(c) (c).begin(),(c).end()
+#define tr(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++)
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+
+
+
+    return 0;
+}
