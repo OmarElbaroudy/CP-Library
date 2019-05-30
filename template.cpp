eff1bffe87be92b6e5459e8be6bb1e21ff2a060d
OmarElbaroudy <elbaroudyomar@gmail.com>
2019-05-30 17:55:37 +0200

    added comments to template

--- template.cpp
+++ template.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include <bits/stdc++.h>
-using namespace std;
 
 const int INF=(int)1e9;
 const double EPS=1e-9;
@@ -12,14 +10,15 @@ typedef pair< int,int > ii;
 typedef vector<ii> vii;
 typedef long long ll;
 #define all(c) (c).begin(),(c).end()
-#define tr(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++)
-#define present(c,x) ((c).find(x) != (c).end())
-#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define tr(c,i) for(typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define present(c,x) ((c).find(x) != (c).end())        //for set or map any container that implements find();
+#define cpresent(c,x) (find(all(c),x) != (c).end())    //for vectors
 
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    cout.tie(nullptr);
 
 
 
