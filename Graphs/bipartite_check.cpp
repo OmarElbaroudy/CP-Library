7fca14782d8b69c0d87ab26a8de4332ffcae0b2a
OmarElbaroudy <elbaroudyomar@gmail.com>
2019-05-30 22:57:15 +0200

    added bipartite check

---
+++ Graphs/bipartite_check.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N=(int) 1e8;
+vector<int> adj[N];
+int color[N];
+
+bool bipartite_check(int u) {
+    for (int v: adj[u]) {
+        if (color[v] == -1) {
+            color[v] = 1 ^ color[u];
+            if (!bipartite_check(v)) return false;
+        } else if (color[v] == color[u]) {
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    memset(color,-1, sizeof(color));
+    color[0]=0;
+    bool valid=bipartite_check(0);
+
+}
