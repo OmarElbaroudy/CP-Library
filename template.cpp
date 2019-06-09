90b1f3e11c8a18e949bcda91a2413676b3c25adc
OmarElbaroudy <elbaroudyomar@gmail.com>
2019-06-09 15:31:36 +0200

    updated template

--- template.cpp
+++ template.cpp
@@ -1,28 +1,29 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-
-const int INF=(int)1e9;
-const double EPS=1e-9;
-typedef vector< int > vi;
-typedef vector< vi > vvi;
-typedef pair< int,int > ii;
+const int INF = (int) 1e9 + 7;
+const double EPS = 1e-9;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
 typedef vector<ii> vii;
 typedef long long ll;
+
 #define all(c) (c).begin(),(c).end()
-#define tr(c,i) for(typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
-#define present(c,x) ((c).find(x) != (c).end())        //for set or map or any container that implements find();
-#define vpresent(c,x) (find(all(c),x) != (c).end())    //for vectors
+#define faster ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define tr(c, i) for(typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define present(c, x) ((c).find(x) != (c).end())        //for set or map or any container that implements find();
+#define vpresent(c, x) (find(all(c),x) != (c).end())    //for vectors
+#define print_array(arr) for (const auto& value: arr) { cout << value << ' ';}
+#define nl "\n"
+#define debug(x) cout<<#x<<" = "<<(x)<<nl
 
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
-
-
-
+    faster;
 
     return 0;
 }
+
+
