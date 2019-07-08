10b4011d2dfb40caa0ba39c32c6d25a861ad0518
OmarElbaroudy <elbaroudyomar@gmail.com>
2019-07-08 18:38:17 +0200

    updated template

--- template.cpp
+++ template.cpp
@@ -2,26 +2,31 @@
 
 using namespace std;
 
-const int INF = (int) 1e9 + 7;
+const int INF = (int) 1e9 - 6;
 const double EPS = 1e-9;
+
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef pair<int, int> ii;
 typedef vector<ii> vii;
 typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
 
 #define all(c) (c).begin(),(c).end()
 #define faster ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
 #define tr(c, i) for(auto i = (c).begin(); i != (c).end(); i++)
 #define present(c, x) ((c).find(x) != (c).end())        //for set or map or any container that implements find();
 #define vpresent(c, x) (find(all(c),x) != (c).end())    //for vectors
-#define print_array(arr) for (const auto& value: arr) { cout << value << ' ';}
+#define print_array(arr, n) for (int i=0; i<n ;i++) { cout << arr[i] << ' ';}
 #define nl "\n"
 #define sp " "
 #define debug(x) cout<<#x<<" = "<<(x)<<nl
+#define read_array(arr, n) for(int i=0; i<n; i++){cin >> arr[i];}
 
 int main() {
     faster;
-
-    return 0;
+    
 }
+
+
